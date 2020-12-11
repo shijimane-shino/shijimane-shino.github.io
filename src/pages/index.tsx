cc0abfebc4c422bf9db841e17054c086c7f07e37
ansanloms <ansanloms@users.noreply.github.com>
2020-12-11 21:37:28 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as Next from "next";
 import Link from "next/link";
-import { Container, Header, List, Button, Image } from "semantic-ui-react";
+import { Header, List, Button, Image } from "semantic-ui-react";
 
 import { MenuList } from "../utils/menu";
 import Layout from "../components/Layout";
@@ -16,56 +16,50 @@ const Index: Next.NextPage<{
   pickups: graphcms.api.Pickup[];
 }> = ({ background, pickups }) => (
   <Layout isHeader={false} isFooter={false}>
-    <Container>
-      <div className={styles.background}>
-        <Image className={styles["background-image"]} src={background.url} />
-      </div>
-      <div className={styles.container}>
-        <div className={styles["title-container"]}>
-          <div className={styles["title-header"]}>
-            <Header as="h1">
-              <Header.Subheader
-                className={styles["title-header-subheading-top"]}
-              >
-                UTAU 向け音声ライブラリ
-              </Header.Subheader>
-              <Header.Content className={styles["title-header-main"]}>
-                <span className={styles["title-header-main-inline"]}>
-                  静寂音
-                </span>
-                <span className={styles["title-header-main-inline"]}>しの</span>
-              </Header.Content>
-              <Header.Subheader
-                className={styles["title-header-subheading-bottom"]}
-              >
-                Shijimane Shino
-              </Header.Subheader>
-            </Header>
-          </div>
-          <div className={styles["link-list"]}>
-            <List horizontal link size="big">
-              {MenuList.filter((menu) => menu.href !== "/").map(
-                (menu, index: number) => (
-                  <Link key={index} href={menu.href}>
-                    <List.Item as="a">{menu.primary}</List.Item>
-                  </Link>
-                )
-              )}
-            </List>
-          </div>
-          <div className={styles["pickup-list"]}>
-            <List horizontal>
-              {pickups.map((pickup) => (
-                <List.Item key={pickup.id}>
-                  <Button href={pickup.url}>{pickup.title}</Button>
-                </List.Item>
-              ))}
-            </List>
-          </div>
-          <div className={styles["copyright"]}>{Copyright}</div>
+    <div className={styles.background}>
+      <Image className={styles["background-image"]} src={background.url} />
+    </div>
+    <div className={styles.container}>
+      <div className={styles["title-container"]}>
+        <div className={styles["title-header"]}>
+          <Header as="h1">
+            <Header.Subheader className={styles["title-header-subheading-top"]}>
+              UTAU 向け音声ライブラリ
+            </Header.Subheader>
+            <Header.Content className={styles["title-header-main"]}>
+              <span className={styles["title-header-main-inline"]}>静寂音</span>
+              <span className={styles["title-header-main-inline"]}>しの</span>
+            </Header.Content>
+            <Header.Subheader
+              className={styles["title-header-subheading-bottom"]}
+            >
+              Shijimane Shino
+            </Header.Subheader>
+          </Header>
         </div>
+        <div className={styles["link-list"]}>
+          <List horizontal link size="big">
+            {MenuList.filter((menu) => menu.href !== "/").map(
+              (menu, index: number) => (
+                <Link key={index} href={menu.href}>
+                  <List.Item as="a">{menu.primary}</List.Item>
+                </Link>
+              )
+            )}
+          </List>
+        </div>
+        <div className={styles["pickup-list"]}>
+          <List horizontal>
+            {pickups.map((pickup) => (
+              <List.Item key={pickup.id}>
+                <Button href={pickup.url}>{pickup.title}</Button>
+              </List.Item>
+            ))}
+          </List>
+        </div>
+        <div className={styles["copyright"]}>{Copyright}</div>
       </div>
-    </Container>
+    </div>
   </Layout>
 );
 
